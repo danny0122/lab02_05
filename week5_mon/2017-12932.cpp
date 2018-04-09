2c7489698d70d1f9359da781cd891f22c9ead9b0
danny0122 <danny0122@naver.com>
2018-04-09 12:22:13 +0900

    2017-12932

---
+++ week5_mon/2017-12932.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+vector<int> l;
+int main() {
+	int n;
+	while(cin>>n){
+		l.push_back(-n);
+	}
+	sort(l.begin(), l.end());
+	cout <<"ordered array" << endl;
+
+	for (auto i : l) {
+		cout << -i << " ";
+	}
+
+}
